1c931b70dda8d8e69a0e38769ce67c8b7e34272b
BenderBolg <Songze@live.cn>
2021-02-27 16:35:39 +0800

    Update 202.口袋抽取球.c

--- XDOJ_C/202.口袋抽取球.c
+++ XDOJ_C/202.口袋抽取球.c
@@ -3,16 +3,8 @@ int main()
 {
 	int red,white,black;
 	for(red=0;red<=3;red++)
-	{
 		for(white=0;white<=4;white++)
-		{
 			for(black=0;black<=5;black++)
-			{
 				if(red+white+black==8) 
-				{
 					printf("%d %d %d\n",red,white,black);
-				}
-			}
-		}
-	}
 }
