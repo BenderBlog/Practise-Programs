4fd498c502f5ae3357a5e2b16d1da52e36c68cd8
David "SuperBart" Chen <Songze@live.cn>
2022-08-18 22:37:01 +0800

    I cannot catch the 5G speed.

---
+++ Helang-PKGBUILD/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'helang',
+    version = '1.0.0',
+    author = 'kifuan',
+    packages = find_packages(),
+    install_requires = [
+        'tqdm'
+        'pytest'
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'helang = helang.__main__:_main',
+        ]
+    },
+)
